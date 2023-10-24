dbac720dce66ba63aea18f0bee4ddcd2b438bf20
BrunoHamon <111573067+BrunoHamon@users.noreply.github.com>
2023-10-24 18:39:32 -0300

    Comentario main.c

--- CNP/main.c
+++ CNP/main.c
@@ -2,8 +2,8 @@
 #include <libprg/libprg.h>
 
 int main(int argc, char *argv[]) {
-    notacao_polonesa_inversa(argc, argv);
     //Chama a fução de calculadora, usando o numero de argumentos e seus valores, via linha de comando.
+    notacao_polonesa_inversa(argc, argv);
 
     return 0;
 }
